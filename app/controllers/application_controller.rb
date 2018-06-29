2488c060b4ec8f777e8ce0b6dd61fcaa478f41c6
abreternitz <aliceburian@gmail.com>
2018-06-29 02:36:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
+require './kwk-l1-sinatra-mvc-file-structure-kwk-students-l1-stl-061818/config/environment'
+require './app/models/model'
 class ApplicationController < Sinatra::Base
 
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
-  end
+  end #sets default framework?
 
-  get "/" do
+  get "/" do #homepage
   	erb :index
   end
 end
